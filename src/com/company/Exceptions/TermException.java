9137ecd3011e07afa8f9ff14c0210a810ec81b6e
Paul-Kijtapart <scc385@gmail.com>
2017-02-28 20:24:26 -0800

    Add Term exceptions

---
+++ src/com/company/Exceptions/TermException.java
@@ -0,0 +1,11 @@
+package com.company.Exceptions;
+
+/**
+ * Created by aor on 2017-02-28.
+ */
+public class TermException extends Exception {
+
+    public TermException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/company/Exceptions/TermFormatException.java
@@ -0,0 +1,11 @@
+package com.company.Exceptions;
+
+/**
+ * Created by aor on 2017-02-28.
+ */
+public class TermFormatException extends TermException {
+
+    public TermFormatException(String message) {
+        super(message);
+    }
+}
