366867287b0803ab8df429228dc7613ff83bb7ce
Paul-Kijtapart <scc385@gmail.com>
2017-03-01 23:33:12 -0800

    Add modes and app to run console

---
+++ src/com/company/FileMode.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by aor on 2017-03-01.
+ */
+public class FileMode {
+
+    public FileMode() {
+        System.out.println("File started.");
+
+    }
+}
---
+++ src/com/company/InteractiveMode.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import com.company.Exceptions.EquationFormatException;
+import com.company.Exceptions.TermException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by aor on 2017-03-01.
+ */
+public class InteractiveMode {
+
+    public InteractiveMode() {
+        System.out.println("InteractiveMode started.");
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                String line = null;
+                System.out.println("Please type a Math Equation or quit if you wish to exit");
+
+                line = in.readLine();
+                if (News360Calculator.isExitCommand(line)) {
+                    System.out.println("Exiting Interactive Mode.");
+                    break;
+                }
+                Equation eq = null;
+                try {
+                    eq = new Equation(line);
+                } catch (EquationFormatException e) {
+                    System.out.println("InteractiveMode : " + e.toString());
+                    continue;
+                } catch (TermException e) {
+                    System.out.println("InteractiveMode : " + e.toString());
+                    continue;
+                }
+                System.out.println("Canonical From : " + eq.toCanonicalString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
